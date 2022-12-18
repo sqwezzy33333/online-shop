fd3d9a5eb7978bde01d8ba16c4cf7b29cc709dee
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2022-12-18 18:22:37 +0300

    feat: add class loader and method load

---
+++ src/components/loader/loader.ts
@@ -0,0 +1,19 @@
+export class Loader {
+  path: string;
+
+  constructor(path: string) {
+    this.path = path;
+  }
+
+  async load() {
+    const data = fetch(this.path)
+      .then((inform) => {
+        if (!inform.ok) {
+          throw new Error();
+        }
+        return inform.json();
+      })
+      .catch((err) => console.log(err));
+    return data;
+  }
+}
