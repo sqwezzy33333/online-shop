acc4e50cb2857372b929a5520463f5b896e8a079
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2022-12-20 22:21:40 +0300

    feat: add method to add cart of product

--- src/components/draw/draw.ts
+++ src/components/draw/draw.ts
@@ -25,10 +25,39 @@ export class DrawElements {
         return await this.loadData.load();
     }
 
+
     async drawCartGoods() {
         const products: IProduct[] = await this.getArrayProducts();
-        const product: IProduct = products[0];
-        console.log(product);
-        return products;
+        for(let i = 0; i < products.length; i++){
+            const product: IProduct = products[i];
+            const catalog = document.querySelector('.main__catalog') as HTMLElement;
+        const div = document.createElement('div') as HTMLElement;
+        div.classList.add("catalog__product");
+        div.classList.add("product");
+        const cart: string = `
+            <div class="product__discount-wrap">
+            <p class="product__discount">-${product.discountPercentage}%</p>
+            </div>
+            <div class="product__image">
+            <img src="${product.thumbnail}" alt="picture"></img>
+            </div>
+            <div class="product__rating-wrap">
+            <p class="product__rating">★${product.rating}</p>
+            </div>
+            <div class="product__title-wrap">
+            <p class="product__title">${product.title}</p>
+            </div>
+            <div class="product__price-wrap">
+            <p class="product__price price">${product.price}$ 
+                <span class="price__old">${Math.round(product.price * (product.discountPercentage / 100) + product.price)}$</span>
+                <span class="price__discount">${Math.round(product.price - (product.price * (product.discountPercentage / 100) + product.price))}$</span>
+            </p>
+            </div>
+            <div class="product__add-wrap">
+            <button class="product__add">Add to cart</button>
+            </div>`;
+        div.innerHTML = cart;
+        catalog.appendChild(div);
+        }
     } 
 }
