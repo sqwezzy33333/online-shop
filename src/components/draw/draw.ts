4bb41406a9cbb79630de8cd927caf4abfa9b229a
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2022-12-22 20:18:11 +0300

    refactor: replace interface to types.ts

--- src/components/draw/draw.ts
+++ src/components/draw/draw.ts
@@ -1,18 +1,5 @@
 import { Loader } from '../loader/loader';
-
-interface IProduct {
-    brand: string;
-    category: string;
-    description: string;
-    discountPercentage: number;
-    id: number;
-    images: string[];
-    price: number;
-    rating: number;
-    stock: number;
-    thumbnail: string;
-    title: string;
-}
+import { IProduct } from '../types/types';
 
 export class DrawElements {
     loadData: Loader;
