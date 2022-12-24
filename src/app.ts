94e9bd1c9d5b92c67352f3d8bce43d2f09741cdb
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2022-12-24 14:05:44 +0300

    feat: add sort.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
 import './libs/normalize.css';
 import './libs/index.scss';
 import { DrawElements } from './components/draw/draw';
+import { Sort } from './components/sort/sort';
 const draw = new DrawElements();
+const sortObj = new Sort();
 console.log(draw.drawCartGoods());
+
+
+const sortClick = document.querySelector<HTMLElement>('.search__button');
+const sortText = document.querySelector<HTMLElement>('.search__text');
+const txt: string = sortText?.outerHTML || '';
+
+sortClick?.addEventListener('click', (e) => {
+    sortObj.sort(txt);
+});
