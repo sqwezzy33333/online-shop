c9a77ce1fe58d0027a930ed797c14a5ceecbb1c4
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2022-12-26 14:18:20 +0300

    feat: add start sort of products

--- src/components/sort/sort.ts
+++ src/components/sort/sort.ts
@@ -1,10 +1,35 @@
 export class Sort {
     sortBlock: HTMLSelectElement;
+    
     constructor(){
         this.sortBlock = document.querySelector('.search__button') as HTMLSelectElement;
     }
 
-    sort(sortType: string){
-        console.log(document.querySelectorAll('.catalog__product'));
+    async sort(sortType: string, arrayProducts?: Element[]){
+        if(sortType == 'rating'){
+            let productsList: Element[];
+            if(arrayProducts === undefined){
+                productsList = [...document.querySelectorAll('.catalog__product')];
+            }
+            else {
+                productsList = arrayProducts;
+            }
+            productsList = productsList.sort((i, j) => {
+                const firstIem = parseFloat(i.querySelector('.product__rating')?.innerHTML.slice(1) as string)
+                const secondItem = parseFloat(j.querySelector('.product__rating')?.innerHTML.slice(1) as string)
+                if (firstIem > secondItem) {
+                    return -1;
+                }
+                if (firstIem < secondItem) {
+                    return 1;
+                }
+                 return 0;
+            });
+            const catalog = document.querySelector('.catalog__products') as HTMLElement;
+            catalog.innerHTML = '';
+            productsList.forEach(function(item){
+                catalog.appendChild(item);
+            });
+        }
     }
 }
