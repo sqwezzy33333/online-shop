d0aacc9cc7369bef0ec9343ccfac8553cfc07fff
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2022-12-26 14:42:55 +0300

    feat: add popup menu for sort

--- src/components/filters/filterCategory.ts
+++ src/components/filters/filterCategory.ts
@@ -1,6 +1,5 @@
-import { IProduct } from '../types/types';
-import { allFilters } from '../forQueryParam/objOfQueryParam';
-import { Filters } from '../types/types';
+import { IProduct, AllFiltersType } from '../../../types/types';
+import { syncURL } from '../forQueryParam/forQueryParam';
 
 export class FilterCategory {
   uploadFilter(): void {
@@ -31,20 +30,23 @@ export class FilterCategory {
     form.className = 'form';
     btnShowAll.className = 'filters__btn';
     btnShowAll.innerHTML = 'Show all';
+
     if (localStorage.getItem('showAllCategories') === 'true') {
       filter.style.height = 'auto';
       btnShowAll.classList.toggle('_active');
       localStorage.setItem('showAllCategories', 'true');
     }
+
     filter.append(title);
     filter.append(btnShowAll);
     filter.append(form);
 
     categoryArray.forEach((category) => {
-<<<<<<< HEAD
       let counter: number = 0;
       const inputRow = document.createElement('div') as HTMLElement;
       inputRow.className = 'filters__input-row';
+      inputRow.classList.add('filters__input-row-category');
+
       if (filtredData !== undefined && filtredData.length < 1) {
         counter = 5;
       } else if (localStorage.getItem('category') === '') {
@@ -56,79 +58,120 @@ export class FilterCategory {
           }
         });
       }
-=======
-      const inputRow = document.createElement('div') as HTMLElement;
-      inputRow.className = 'filters__input-row';
-      const countOfProducts = productsArray.filter((item) => item.category === category);
->>>>>>> 8ac8359 (feat: add popup menu for sort)
+
+      if (
+        localStorage.getItem('brand') ||
+        localStorage.getItem('rightPriceValue') !== '' ||
+        localStorage.getItem('leftPriceValue') !== '' ||
+        localStorage.getItem('leftStockValue') !== '' ||
+        localStorage.getItem('rightStockValue') !== ''
+      ) {
+        counter = 0;
+        filtredData?.forEach((el) => {
+          if (el.category === category) {
+            counter++;
+          }
+        });
+      }
+
       inputRow.innerHTML = `
                             <input type="checkbox" id="${category}" name='category'>
                             <label for="${category}">${category}
                             </label><span class="filters__counter">${counter}/5</span>`;
+      if (counter === 0) {
+        inputRow.classList.add('row-null');
+      }
+
+      if (
+        localStorage.getItem('brand') !== '' &&
+        localStorage.getItem('brand') !== null &&
+        inputRow.classList.contains('row-null')
+      ) {
+        filtredData?.forEach((el) => {
+          if (el.category !== category) {
+            inputRow.classList.add('row-disable');
+          }
+        });
+      }
       form?.append(inputRow);
     });
     filters?.append(filter);
   }
 
-  checkFilter(): void {
-    const filterRow = document.querySelectorAll('.filters__input-row');
-    function transformToURLParams(filters: Filters) {
-      const query = Object.entries(filters)
-        .map(([key, value]) => {
-          return `${key}=${value}`;
-        })
-        .join('&');
-      return `?${query}`;
-    }
-    function syncURL(filters: Filters) {
-      const path = document.location.pathname;
-      const query = transformToURLParams(filters);
-
-      window.history.pushState(filters, '', `${path}${query}`);
-      window.history.pushState(filters, '', `${path}${query}`);
-      history.back();
-    }
+  checkFilter(allFiltersOnload?: AllFiltersType): void {
+    const filterRow = document.querySelectorAll('.filters__input-row-category');
     filterRow.forEach((item) => {
       const input = item.children[0] as HTMLInputElement;
       input.addEventListener('change', function () {
         if (input.checked) {
           const localStorageCategory = localStorage.getItem('category');
-          if (localStorageCategory) {
-            allFilters.category = localStorageCategory;
+          const searchClear = location.search.split('');
+          searchClear.shift();
+          const queryParamsString = searchClear.join('').toString();
+          let paramsObject;
+          if (queryParamsString !== '') {
+            paramsObject = JSON.parse(
+              '{"' + decodeURI(queryParamsString).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}'
+            );
           }
-          allFilters.category += `,${input.id}`;
-          localStorage.setItem('category', allFilters.category);
-          syncURL(allFilters);
-          item.children[1].classList.toggle('cheked');
+          if (paramsObject !== undefined) {
+            if (localStorageCategory) {
+              paramsObject.category = localStorageCategory;
+            }
+            if (paramsObject.category.indexOf(input.id) === -1) {
+              paramsObject.category += `%2C${input.id}`;
+            }
+            localStorage.setItem('category', paramsObject.category);
+            syncURL(paramsObject);
+            item.children[1].classList.toggle('cheked');
+          }
         } else {
-          const arrayFromCategory = allFilters.category.split(',');
-          const filtredArrayOfCategory = arrayFromCategory.filter((element) => {
-            return element !== input.id && element !== '';
-          });
-          allFilters.category = filtredArrayOfCategory.toString();
-          syncURL(allFilters);
-          localStorage.setItem('category', allFilters.category);
-          item.children[1].classList.toggle('cheked');
+          const searchClear = location.search.split('');
+          searchClear.shift();
+          const queryParamsString = searchClear.join('').toString();
+          let paramsObject;
+          if (queryParamsString !== '') {
+            paramsObject = JSON.parse(
+              '{"' + decodeURI(queryParamsString).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}'
+            );
+          }
+          if (paramsObject !== undefined) {
+            let arrayFromCategory = paramsObject.category.split('%2C');
+            if (allFiltersOnload !== undefined) {
+              arrayFromCategory = allFiltersOnload.category.split('%2C');
+            }
+            const filtredArrayOfCategory = arrayFromCategory.filter((element: string) => {
+              return element !== input.id && element !== '';
+            });
+            paramsObject.category = '%2C' + filtredArrayOfCategory.toString().replace(/,/g, '%2C');
+            if (paramsObject.category === '%2C') paramsObject.category = '';
+            syncURL(paramsObject);
+            localStorage.setItem('category', paramsObject.category);
+            item.children[1].classList.toggle('cheked');
+          }
         }
       });
     });
   }
 
-  drawChekedInput(): void {
-    const filterRow = document.querySelectorAll('.filters__input-row');
-    const arrayFromCategory = allFilters.category.split(',');
-    const filtredArrayOfCategory = arrayFromCategory.filter((element) => {
-      return element !== '';
-    });
-    filterRow.forEach((item) => {
-      const input = item.children[0] as HTMLInputElement;
-      filtredArrayOfCategory.forEach((el) => {
-        if (el === input.id) {
-          item.children[1].classList.toggle('cheked');
-          input.checked = true;
-        }
+  drawChekedInput(allFiltersOnload?: AllFiltersType): void {
+    const filterRow = document.querySelectorAll('.filters__input-row-category');
+    let arrayFromCategory;
+    if (allFiltersOnload?.category !== '') {
+      arrayFromCategory = allFiltersOnload?.category.split('%2C');
+      const filtredArrayOfCategory = arrayFromCategory?.filter((element) => {
+        return element !== '';
       });
-    });
+      filterRow.forEach((item) => {
+        const input = item.children[0] as HTMLInputElement;
+        filtredArrayOfCategory?.forEach((el) => {
+          if (el === input.id) {
+            item.children[1].classList.toggle('cheked');
+            input.checked = true;
+          }
+        });
+      });
+    }
   }
 
   openAllFilters() {
@@ -141,6 +184,7 @@ export class FilterCategory {
         localStorage.setItem('showAllCategories', 'true');
       } else {
         filter.style.height = '180px';
+        localStorage.setItem('showAllCategories', 'false');
       }
     });
   }
