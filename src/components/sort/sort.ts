f291db08ea08c810ad14c2d2943f9a122886f166
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2022-12-26 14:18:20 +0300

    feat: add start sort of products

--- src/components/sort/sort.ts
+++ src/components/sort/sort.ts
@@ -3,6 +3,7 @@ import { Filters } from '../types/types'
 
 export class Sort {
     sortBlock: HTMLSelectElement;
+    
     constructor(){
         this.sortBlock = document.querySelector('.catalog__search') as HTMLSelectElement;
     }
