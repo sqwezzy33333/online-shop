c27c07c25d7ad39586266f105ca0ba00d9f4f013
Sqwezzy33333 <sergey.kornev3@yandex.by>
2023-01-08 09:42:23 +0300

    fix: change onloading range inputs

--- src/components/filters/clear.ts
+++ src/components/filters/clear.ts
@@ -1,13 +1,26 @@
+import { AllFiltersType } from '../../types/types';
+import { syncURL } from '../../components/forQueryParam/forQueryParam';
 export class Clear {
   buttonClear: HTMLSelectElement;
-  constructor(){
-      this.buttonClear = document.querySelector('.clear-btn') as HTMLSelectElement;
+  constructor() {
+    this.buttonClear = document.querySelector('.clear-btn') as HTMLSelectElement;
   }
 
-  async clearFilters(){
-      this.buttonClear.addEventListener('click', ()=> {
-        localStorage.clear();
-        
-      });
+  async clearFilters() {
+    this.buttonClear.addEventListener('click', () => {
+      localStorage.clear();
+      let search = document.querySelector('.search__input') as HTMLInputElement;
+      search.value = '';
+      let clearObject: AllFiltersType = {
+        category: '',
+        brand: '',
+        price: '',
+        stock: '',
+        search: '',
+        type: 'priceUp',
+        view: 'blocks'
+      };
+      syncURL(clearObject);
+    });
   }
 }
