c0e9e4151be6a3350a5f1cefd6cefdf69b185c89
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2023-01-11 21:31:07 +0300

    fix: fix clear button

--- src/pages/main/components/filters/clear.ts
+++
@@ -1,9 +0,0 @@
-export class Clear {
-
-  async clearFilters(){
-    const buttonClear = document.querySelector('.clear-btn') as HTMLSelectElement;
-      buttonClear.addEventListener('click', ()=> {
-        localStorage.clear();
-      });
-  }
-}
